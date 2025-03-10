6e16a9c3e59e10774718757cacb850efbed871d9
Emerson Millard <emillard.code@gmail.com>
2025-03-10 12:00:00 -0700

    Added challenge attempt and solution unit tests.

---
+++ src/test/java/com/project/LeetCodeAttemptTest.java
@@ -0,0 +1,31 @@
+package com.project;
+
+import com.project.attempt.LeetCodeAttempt;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LeetCodeAttemptTest {
+
+    @Test
+    public void mergeTwo2DArraysBySummingValuesTest() {
+
+        int[][] nums1 = { {1, 2}, {2, 3}, {4, 5} };
+        int[][] nums2 = { {1, 4}, {3, 2}, {4, 1} };
+        int[][] nums3 = { {1, 6}, {2, 3}, {3, 2}, {4, 6} };
+        int[][] nums4 = LeetCodeAttempt.mergeTwo2DArraysBySummingValues(nums1, nums2);
+
+        assertArrayEquals(nums3, nums4);
+
+        int[][] nums5 = { {2, 4}, {3, 6}, {5, 5} };
+        int[][] nums6 = { {1, 3}, {4, 3} };
+        int[][] nums7 = { {1, 3}, {2, 4}, {3, 6}, {4, 3}, {5, 5} };
+        int[][] nums8 = LeetCodeAttempt.mergeTwo2DArraysBySummingValues(nums5, nums6);
+
+        assertArrayEquals(nums7, nums8);
+
+    }
+
+
+
+}
